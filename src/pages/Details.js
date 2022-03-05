4e2f802acb05c593d3273cf56a8d068435bc62a0
piagetgit <40717351+piagetgit@users.noreply.github.com>
2022-03-05 02:41:42 +0100

    Fixed caroussel

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -37,26 +37,27 @@ function Details() {
     return (
         <Container>
             <Row className="justify-content-md-center">
-                <Col sm >
-                    <Carousel style={{'width':"400px"}} fade >
+                <Col sm>
+                    <Carousel fade >
                         <Carousel.Item>
-                            <Card.Img id={classes.image}  src={loadedBraid.urlImage} alt="First slide" />
+                            <Card.Img id={classes.image}  className="d-block mw-100" src={loadedBraid.urlImage} alt="First slide" />
                             <Carousel.Caption>
-                                <h3>First Image</h3>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-
                         <Carousel.Item>
-                            <Card.Img id={classes.image}  src={loadedBraid.urlImage} alt="Second slide" />
+                        <Card.Img id={classes.image}  className="d-block mw-100" src={loadedBraid.urlImage} alt="Second slide" />
                             <Carousel.Caption>
-                               <h3>Second Image</h3>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-
                         <Carousel.Item>
-                            <Card.Img id={classes.image}  src={loadedBraid.urlImage} alt="Third slide" />
+                        <Card.Img id={classes.image}  className="d-block w-100" src={loadedBraid.urlImage} alt="Third slide" />
                             <Carousel.Caption>
-                                <h3>Third Image</h3>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
